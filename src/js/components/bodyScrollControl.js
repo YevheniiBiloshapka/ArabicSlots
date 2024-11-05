0b013133a8bb44f8ff4c9900a57d2820883a17df
Yevhenii Biloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2024-11-05 21:13:34 +0100

    fix open modal

--- src/js/components/bodyScrollControl.js
+++ src/js/components/bodyScrollControl.js
@@ -1,26 +1,30 @@
 export default function controlBodyScroll(isOpen) {
   const body = document.querySelector('body');
-  const html = document.querySelector('html');
   const header = document.querySelector('header');
 
-  function getScrollbarWidth() {
-    return window.innerWidth - document.documentElement.clientWidth;
-  }
+  const scrollbarWidth = window.innerWidth - document.body.offsetWidth + 'px';
 
-  const scrollbarWidth = getScrollbarWidth();
 
   if (isOpen) {
 
-    body.classList.add('modal-open');
-    html.style.overflowY = 'hidden';
     if (window.innerWidth > 768) {
-      body.style.paddingRight = `${scrollbarWidth}px`;
-      header.style.paddingRight = `${scrollbarWidth}px`;
+      body.style.paddingRight = scrollbarWidth;
+      header.style.paddingRight = scrollbarWidth;
     }
+
+    let pagePosition = window.scrollY;
+
+    body.classList.add('modal-open');
+    body.dataset.position = pagePosition;
+    body.style.top = -pagePosition + 'px';
   } else {
+    let pagePosition = parseInt(document.body.dataset.position, 10);
     body.style.paddingRight = '';
     header.style.paddingRight = '';
+    
+    body.style.top = 'auto';
     body.classList.remove('modal-open');
-    html.style.overflowY = '';
+    window.scroll({ top: pagePosition, left: 0 });
+    body.removeAttribute('data-position');
   }
 }
