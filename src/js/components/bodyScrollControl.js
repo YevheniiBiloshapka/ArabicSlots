0ca2f2a646ba18f33b303805f5c51ea6c022d7b8
Yevhenii Biloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2024-11-07 12:22:54 +0100

    add

--- src/js/components/bodyScrollControl.js
+++ src/js/components/bodyScrollControl.js
@@ -1,12 +1,10 @@
 export default function controlBodyScroll(isOpen) {
   const body = document.querySelector('body');
   const header = document.querySelector('header');
-
   const scrollbarWidth = window.innerWidth - document.body.offsetWidth + 'px';
 
 
   if (isOpen) {
-
     if (window.innerWidth > 768) {
       body.style.paddingRight = scrollbarWidth;
       header.style.paddingRight = scrollbarWidth;
@@ -21,7 +19,7 @@ export default function controlBodyScroll(isOpen) {
     let pagePosition = parseInt(document.body.dataset.position, 10);
     body.style.paddingRight = '';
     header.style.paddingRight = '';
-    
+
     body.style.top = 'auto';
     body.classList.remove('modal-open');
     window.scroll({ top: pagePosition, left: 0 });
